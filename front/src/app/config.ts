9d3355cc5e277cb565adb6ce207666a05aebcd8c
jpgonzalezo <juan.gonzalez.o@usach.cl>
2019-12-05 15:28:11 -0300

    FRONT UPDATE: configuracion de angular

--- front/src/app/config.ts
+++ front/src/app/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
 	API_SERVER_URL: 'http://api.vrkids.nice.cl',
-	API_PREVIEW_PROFESOR: 'http://qa.vrkids.nice.cl/web_vr'
+	API_PREVIEW_PROFESOR: 'http://vrforkid.org/web_vr'
 }
